49413292d439417700ba3ae991258c92fb995fb6
fysxaki <baetsu314@gmail.com>
2022-10-23 00:03:05 +0800

    4th commit

--- lodash/fysxaki-lodash.js
+++ lodash/fysxaki-lodash.js
@@ -1,7 +1,30 @@
 var fysxaki = function () {
 
-  function add(a, b) {
+  function chunk(array, size = 1) {
+    let resultAry = []
+    let itemAry = []
+    let count = 0
 
+    for (let i = 0; i < array.length; i++) {
+        count++
+        if (count <= size) {
+            itemAry.push(array[i])
+        } else if (count > size) {
+            count = 0
+            resultAry.push(itemAry.slice())
+            itemAry = []
+            itemAry.push(array[i])
+        }
+    }
+
+    if (itemAry.length) {
+        resultAry.push(itemAry.slice())
+    }
+
+    return resultAry
+  }
+
+  function add(a, b) {
     return a + b
   }
 }
